e83109a4355f79567afba0968784953befebb664
daniela-park <daniela-park@outlook.com>
2024-05-27 00:27:10 +0100

    Code split into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
-import MarathonResults from "../src/MarathonResults.json";
-import Running from "../public/running.gif"
+import Data from "../src/MarathonResults.json";
+import Running from "/running.gif"
+import Table from './components/Table';
+import NavBarTop from './components/NavBarTop';
 
 // https://girraphic.com/about/
 
@@ -9,42 +11,17 @@ function App() {
   return (
     <main>
 
+      {/* <NavBarTop></NavBarTop> */}
+
       <header>
+      <h1>
         <img src={Running} height="50px" alt="Animated gif of a person running" />
-        <h1>
-          {MarathonResults.results.racename}
+        {} {Data.results.racename}
         </h1>
       </header>
       <hr />
 
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">Rank</th>
-            <th scope="col">Bib</th>
-            <th scope="col">Time</th>
-            <th scope="col">Name</th>
-            <th scope="col">Country</th>
-          </tr>
-        </thead>
-
-        {MarathonResults && MarathonResults.results.athletes.map((result) => (
-          <tbody>
-            <tr key={result.athleteid}>
-              <th className="numbers" scope="row">{result.rank}</th>
-              <td className="numbers">{result.bibnumber}</td>
-              <td className="numbers">{result.finishtime}</td>
-              <td className="string">{result.firstname} { } {result.surname.toUpperCase()}</td>
-              <td className="string">{result.flag} {}
-              <img 
-                src={`https://assets.thebasetrip.com/api/v2/countries/flags/${result.countryname.toLowerCase().replace(' ', '-')}.png`}
-                height="15px"
-                alt={result.countryname}/>
-              </td>
-            </tr>
-          </tbody>
-        ))}
-      </table>
+      <Table />
 
       <hr />
       <footer>
